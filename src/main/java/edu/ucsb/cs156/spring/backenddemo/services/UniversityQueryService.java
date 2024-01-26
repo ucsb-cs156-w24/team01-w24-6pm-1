68926d52f76c954c6abe3c465303b6a88d49fe73
Jenil Prajapati <jenilrajeshkumar@ucsb.edu>
2024-01-25 18:42:39 -0800

    cg - added service to get information on zip codes

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
@@ -1,32 +1,34 @@
 package edu.ucsb.cs156.spring.backenddemo.services;
 
-
-
 import com.fasterxml.jackson.databind.ObjectMapper;
-
-import org.springframework.web.client.RestTemplate;
-
 import lombok.extern.slf4j.Slf4j;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Service
 public class UniversityQueryService {
 
-
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
 
-    public UniversityQueryService(RestTemplateBuilder restTemplateBuilder) {
-        restTemplate = restTemplateBuilder.build();
+    public UniversityQueryService(RestTemplateBuilder restTemplateBuilder, ObjectMapper objectMapper) {
+        this.restTemplate = restTemplateBuilder.build();
+        this.objectMapper = objectMapper;
     }
 
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "http://universities.hipolabs.com/search?name={name}";
 
     public String getJSON(String name) throws HttpClientErrorException {
-       return "";
+        String url = ENDPOINT.replace("{name}", name);
+        try {
+            return restTemplate.getForObject(url, String.class);
+        } catch (HttpClientErrorException e) {
+            log.error("Error while fetching university data: {}", e.getMessage());
+            throw e;
+        }
     }
 }
